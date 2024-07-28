0d15d556a8393f1f5ff7bfa89ef371be16b9f58e
AshenSachinthana <dasachinthana@gmail.com>
2024-07-28 10:30:22 +0530

    Initial commit

---
+++ src/main/java/com/communicare/CommuniCareBackend/CommuniCareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.communicare.CommuniCareBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuniCareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuniCareBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/communicare/CommuniCareBackend/CommuniCareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communicare.CommuniCareBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuniCareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
