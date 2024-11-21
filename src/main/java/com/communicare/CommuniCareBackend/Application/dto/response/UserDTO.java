e6aa30c23d19d452dc2a3fc03889503f5962f204
AshenSachinthana <dasachinthana@gmail.com>
2024-11-21 23:40:08 +0530

    change

--- src/main/java/com/communicare/CommuniCareBackend/Application/dto/response/UserDTO.java
+++ src/main/java/com/communicare/CommuniCareBackend/Application/dto/response/UserDTO.java
@@ -3,7 +3,6 @@ package com.communicare.CommuniCareBackend.Application.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @NoArgsConstructor
 @AllArgsConstructor
