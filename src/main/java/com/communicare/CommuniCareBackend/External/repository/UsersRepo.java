a3bbdbb57dafc8a9c1b9b3521cceb2f4d328ae56
Raveesha Weerasinghe <136602532+Raveesha-dw@users.noreply.github.com>
2024-11-24 08:38:16 +0530

    Initial Commit

--- src/main/java/com/communicare/CommuniCareBackend/External/repository/UsersRepo.java
+++ src/main/java/com/communicare/CommuniCareBackend/External/repository/UsersRepo.java
@@ -1,6 +1,6 @@
 package com.communicare.CommuniCareBackend.External.repository;
 
-import com.communicare.CommuniCareBackend.Domain.entity.Users;
+import  com.communicare.CommuniCareBackend.Domain.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
