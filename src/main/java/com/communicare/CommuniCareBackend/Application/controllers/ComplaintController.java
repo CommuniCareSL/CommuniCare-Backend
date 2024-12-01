da6d81640bd571097321c0a36e9fd4513aae6949
AshenSachinthana <dasachinthana@gmail.com>
2024-12-01 21:36:47 +0530

    test

--- src/main/java/com/communicare/CommuniCareBackend/Application/controllers/ComplaintController.java
+++ src/main/java/com/communicare/CommuniCareBackend/Application/controllers/ComplaintController.java
@@ -4,6 +4,7 @@ package com.communicare.CommuniCareBackend.Application.controllers;
 import com.communicare.CommuniCareBackend.Domain.entity.Complaint;
 import com.communicare.CommuniCareBackend.Domain.service.ComplaintService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/app/api/user/complaints")
 @RequiredArgsConstructor
@@ -23,6 +25,7 @@ public class ComplaintController {
     public ResponseEntity<?> submitComplaint(@RequestBody Map<String, Object> complaintData) {
         try {
             Complaint complaint = complaintService.processComplaintData(complaintData);
+            //log.info("Received request to submit complaint: {}", complaintData);
             return ResponseEntity.ok(complaint);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
