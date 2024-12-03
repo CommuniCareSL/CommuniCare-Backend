942bceee97eb140a6a1e7d6310f54085d393896a
Raveesha Weerasinghe <136602532+Raveesha-dw@users.noreply.github.com>
2024-12-04 00:58:51 +0530

    complaint view files #5

--- src/main/java/com/communicare/CommuniCareBackend/External/repository/UserRepository.java
+++ src/main/java/com/communicare/CommuniCareBackend/External/repository/UserRepository.java
@@ -12,4 +12,5 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
     Optional<User> findByIdNumber(String idNumber);
+
 }
